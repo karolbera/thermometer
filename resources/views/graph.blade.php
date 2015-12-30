f618d464bee15ae8a4ec8171bc38a1de5694904f
Bogdan Juszczak <bogdan.juszczak@makingwaves.com>
2015-12-30 11:12:04 +0100

    update

--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="refresh" content="10" >
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel</title>
+    <title>Thermometer</title>
 
     <link rel="stylesheet" href="{!! URL::asset('css/style.css'); !!}">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,900" rel="stylesheet" type="text/css">
