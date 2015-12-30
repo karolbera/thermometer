0e3a65a2f2b003629b4daca204d4ae1d5d89b94c
Bogdan Juszczak <bogdan.juszczak@makingwaves.com>
2015-12-30 09:27:00 +0100

    update

--- app/Http/Controllers/CheckTempController.php
+++ app/Http/Controllers/CheckTempController.php
@@ -5,20 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use \Illuminate\Support\Facades\Config;
 
 class CheckTempController extends Controller
 {
 
+    private $amountOfChecks;
     private $errors;
 
     public function __construct()
     {
+        $this->amountOfChecks = Config::get('config.amountOfChecks');
         $this->errors = app('App\Console\Commands\StoreTemperatureCommand')->checkLogsForErrors();
     }
 
     public function tooHot()
     {
-        if ($this->errors['too_hot'] >= 5
+        if ($this->errors['too_hot'] >= $this->amountOfChecks
             && $this->errors['too_cool'] == 0) {
             echo 'too hot';
         } else {
@@ -26,9 +29,15 @@ class CheckTempController extends Controller
         }
     }
 
+    public function current()
+    {
+        $temp = app('App\Console\Commands\StoreTemperatureCommand')->getTemperature();
+        echo $temp . ' &deg;C';
+    }
+
     public function tooCool()
     {
-        if ($this->errors['too_cool'] >= 5
+        if ($this->errors['too_cool'] >= $this->amountOfChecks
             && $this->errors['too_hot'] == 0) {
             echo 'too cool';
         } else {
@@ -38,7 +47,7 @@ class CheckTempController extends Controller
 
     public function thermometer()
     {
-        if ($this->errors['thermometer_not_working'] >= 5
+        if ($this->errors['thermometer_not_working'] >= $this->amountOfChecks
             && $this->errors['too_hot'] == 0
             && $this->errors['too_cool'] == 0) {
             echo 'can\'t access thermometer';
